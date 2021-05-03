c1e6c86d8d82ab21bee597a998d52a025b26ac8c
Harlan Wilton <harlan@harlanzw.com>
2021-05-03 20:45:49 +1000

    chore: upgrade deps windicss-webpack-plugin @ 0.4.1

--- test/fixture/src/pages/About.vue
+++ test/fixture/src/pages/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <Layout>
+  <Layout class="">
     <h1>About us</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
   </Layout>
--- test/fixture/src/pages/Index.vue
+++ test/fixture/src/pages/Index.vue
@@ -1,18 +1,20 @@
 <template>
 <Layout>
 
-  <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-  <g-image alt="Example image" src="~/favicon.png" width="135" />
+  <div class="container mx-auto max-w-screen-lg">
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-  <h1 class="text-green-700 text-2xl mt-5 mb-3 font-bold">Hello, world!</h1>
+    <h1 class="text-green-700 bg-blue-100 rounded p-2 text-2xl mt-5 mb-3 font-bold">Hello, world!</h1>
 
-  <p class="text-gray-700 text-lg mb-3">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-  </p>
+    <p class="text-gray-700 text-lg mb-3">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+    </p>
 
-  <a class="underline text-blue-600 mr-3" href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-  <a class="underline text-blue-600" href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    <a class="underline text-blue-600 mr-3" href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+    <a class="underline text-blue-600" href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
 
+  </div>
 </Layout>
 </template>
 
